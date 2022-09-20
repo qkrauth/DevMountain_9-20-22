a273100b24dc827bfe7229c71756c2fde71abc08
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-20 10:31:38 -0400

    new day

---
+++ challenge-find-lucky-nums/findLuckyNumbers.js
@@ -0,0 +1,17 @@
+// Given a number n, return an array containing n unique random numbers 1-10. dont repeat numbers
+// Write your code below this line.
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function randomizer(n) {
+    for (let i = 0; i < n; i++) {
+        return array[i];
+    }
+}
+console.log(randomizer(5));
+
+
+// Solution
+function randomizer1(n) {
+    let lucky_array = [];
+    return 
+}
